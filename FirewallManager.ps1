6a637981d996570f2982a81928bdc24677026bf9
Liam Powell <Powell.liam.p@gmail.com>
2022-02-28 01:01:43 -0600

    Update FirewallManager.ps1
    again with the ports

--- FirewallManager.ps1
+++ FirewallManager.ps1
@@ -65,7 +65,7 @@ function Documentation(){
 # Create the output information
 function FirewallRoles($Role){
 	
-	$AD =@('88','135','138','139','389','464','636','3268','3269')
+	$AD =@('88','135','138','139','389','445','464','636','3268','3269')
 	$DHCP =@('647')
 	$DHCPUDP =@('67','547','647','847')
 	$DNS =@('53')
@@ -110,8 +110,8 @@ function FirewallInit(){
 	#outbound not inbound
 	#80, 8080, 443, 
 	
-	$SecurityBlocks =@('445','3389','22','5300')
-	$SecRangeBlocks =@('1-52','54-66','68-79','81-87','89-109','111-122','124-134','136-137','140-142','144-388','390-442','444-463','465-546','548-586','588-635','637-646','648-846','848-992','993-994','996-3267','3270-8079','8081-49151')
+	$SecurityBlocks =@('3389','22','5300')
+	$SecRangeBlocks =@('1-52','54-66','68-79','81-87','89-109','111-122','124-134','136-137','140-142','144-388','390-442','444','446-463','465-546','548-586','588-635','637-646','648-846','848-992','993-994','996-3267','3270-8079','8081-49151')
 	#examine notes for updated ranges
 	
 	foreach($x in $SecurityBlocks){New-NetFirewallrule -DisplayName "Block Port $x" -Direction Inbound -LocalPort $x -Protocol TCP -Action Block
